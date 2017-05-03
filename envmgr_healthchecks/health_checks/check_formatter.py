4f7bd08920532dd93ebd69612d5ef9655a3067ca
jake-cross <jake.cross@thetrainline.com>
2017-05-03 12:50:38 +0100

    deep copies made of the check before altering in the iteritems

--- envmgr_healthchecks/health_checks/check_formatter.py
+++ envmgr_healthchecks/health_checks/check_formatter.py
@@ -1,19 +1,25 @@
 """ Check Formatter """
 
+import copy
+
 
 class CheckFormatter(object):
     """ Check Formatter """
+
     def __init__(self):
         self.check = None
+        self.info = None
 
     def format(self, check, info):
         """ Call all the required formatting functions on the check """
-        self.check = check
-        self._format_http(check, info)
-        return check
+        self.check = copy.deepcopy(check)
+        self.info = copy.deepcopy(info)
+        self._format_http()
+        return self.check
 
-    def _format_http(self, check=None, info=None):
-        if 'http' in check:
-            self.check['http'] = check['http'].replace("${PORT}", info['port'])
+    def _format_http(self):
+        if 'http' in self.check:
+            self.check['http'] = self.check['http'].replace("${PORT}",
+                                                            str(self.info['port']))
         else:
             pass
